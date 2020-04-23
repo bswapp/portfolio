889aeeef09f08415820a5969f1c89c52dc55629c
bswapp <brookeswapp@gmail.com>
2020-04-22 21:12:39 -0600

    test

---
+++ src/components/contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Let's Talk</h1>
+      <div>brookeswapp@gmail.com</div>
+      <div>801-360-0490</div>
+      <div>https://github.com/bswapp</div>
+      <div>https://www.linkedin.com/in/brooke-swapp/</div>
+    </div>
+  )
+}
+export default Contact
